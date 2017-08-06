68cd311a6ec4d7a443c584a083b497391acb2d84
len <len@kanade.eu>
2017-08-06 16:09:29 +0200

    Fix include. Update gradle

--- library/src/main/cpp/crop.h
+++ library/src/main/cpp/crop.h
@@ -1,7 +1,7 @@
 #ifndef CROP_H
 #define CROP_H
 
-#include <cstdlib>
+#include <cstring>
 
 void cropBitmap_ARGB_8888(void *pixels, uint32_t sourceWidth,
                           uint32_t left, uint32_t top, uint32_t width, uint32_t height);
